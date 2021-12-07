c9198be5cd867eda2dce58e0ecb4ba49168a4cc2
rupertjamessimpson <87886963+rupertjamessimpson@users.noreply.github.com>
2021-12-07 12:28:15 -0500

    Trying to prep for heroku with AWS down

--- app/javascript/react/components/HomePage.js
+++ app/javascript/react/components/HomePage.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import logo from '/Users/rupertsimpson/challenges/breakable-toy/app/assets/images/logo.png';
-import rupertspokemon from '/Users/rupertsimpson/challenges/breakable-toy/app/assets/images/rupertspokemon.png'
 
 const HomePage = () => {
 
   return (
     <div className='home-container'>
       <div className='logo'>
-        <img src={logo} />
+Logo Image
       </div>
       <div className='rupe'>
-        <img src={rupertspokemon} 
-        width="400"
-        height="300"
-        />
+Page Image (w400 h300)
       </div>
     </div>
   )
