6df589730da99dd6ea111c0ea6e00f86257b73a6
rupertjamessimpson <87886963+rupertjamessimpson@users.noreply.github.com>
2021-12-15 15:59:09 -0500

    Added about the developer page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :pokemon, only: [:index, :show, :create]
   resources :usage, only: [:index]
+  resources :developer, only: [:index]
 
   namespace :api do
     namespace :v1 do
